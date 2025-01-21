7a6cdcb25bb6582d8edc492b77431a54f7397e32
Nita Krasniqi <psynk8@exmail.nottingham.ac.uk>
2025-01-21 12:11:35 +0000

    NavBar Scrolling on Mobile Fix + Highlighting the Section You Are On

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,50 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'; // Use NavLink instead of Link
 
 const Navbar = () => {
   return (
     <nav style={styles.navbar}>
       <ul style={styles.navList}>
-        <li><Link to="/" style={styles.link}>Welcome</Link></li>
-        <li><Link to="/about" style={styles.link}>About</Link></li>
-        <li><Link to="/practice-description" style={styles.link}>Practice Description</Link></li>
-        <li><Link to="/session-details" style={styles.link}>Session Details</Link></li>
-        <li><Link to="/contact" style={styles.link}>Contact</Link></li>
+        <li>
+          <NavLink 
+            to="/" 
+            style={({ isActive }) => isActive ? { ...styles.link, ...styles.activeLink } : styles.link}
+          >
+            Welcome
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to="/about" 
+            style={({ isActive }) => isActive ? { ...styles.link, ...styles.activeLink } : styles.link}
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to="/practice-description" 
+            style={({ isActive }) => isActive ? { ...styles.link, ...styles.activeLink } : styles.link}
+          >
+            Practice Description
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to="/session-details" 
+            style={({ isActive }) => isActive ? { ...styles.link, ...styles.activeLink } : styles.link}
+          >
+            Session Details
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to="/contact" 
+            style={({ isActive }) => isActive ? { ...styles.link, ...styles.activeLink } : styles.link}
+          >
+            Contact
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
@@ -30,11 +65,14 @@ const styles = {
     padding: '0',
   },
   link: {
-    color: '#333',
+    color: '#333',  // Default color
     textDecoration: 'none',
     padding: '10px 20px',
-    display: 'block'
-  }
+    display: 'block',
+  },
+  activeLink: {
+    color: '#a6a6a6',  // Active link color (blue or any color you prefer)
+  },
 };
 
 export default Navbar;
