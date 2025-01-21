484c3a84e035172686e20a5868f24baae8596135
Nita Krasniqi <psynk8@exmail.nottingham.ac.uk>
2025-01-21 14:30:43 +0000

    Removed Website Made By Nita Krasniqi

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,6 @@ const Footer = () => {
   return (
     <footer style={styles.footer}>
       <p>Susan Steinberg Psychotherapist</p>
-      <p style={styles.footerText}>Website Made by Nita Krasniqi</p>
     </footer>
   );
 };
@@ -12,7 +11,7 @@ const Footer = () => {
 const styles = {
   footer: {
     background: '#cceef920',
-    color: '#333',
+    color: '#90a9b2',
     padding: '20px 0',
     textAlign: 'center',
     marginTop: 'auto', // Ensures the footer stays at the bottom when content is short
