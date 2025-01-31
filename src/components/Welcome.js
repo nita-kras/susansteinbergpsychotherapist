3bdbb42da6b85067279f035ba29a8a85caf6573a
Nita Krasniqi <psynk8@exmail.nottingham.ac.uk>
2025-01-31 11:18:22 +0000

    Final Draft Text & Reposition Image

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -23,14 +23,15 @@ const Welcome = () => {
         <div className="textContent">
           <div className="trainingHeader">Welcome</div>
           <p>
-            Change is never easy, but with the right help, you can make your journey easier. 
-            My approach has a relational-developmental psychodynamic core, integrated with 
-            EMDR for patients who have experienced trauma.
+            Change is never easy but with the right help you can make your journey not only easier but more rewarding. 
+            I am a psychotherapist offering in-person and online therapy in London NW3. My approach has a relational-developmental psychodynamic core, that is integrated with EMDR for patients who have experienced trauma.
+            
           </p>
           <p>I work with adults of all ages who are trying to cope with a myriad of issues:</p>
-          <p>Relationships, Anxiety, Depression, Illness</p>
-          <p>Acute, Chronic, or Complex Trauma (PTSD)</p>
-          <p>Long-term effects of growing up with mentally dysfunctional or unwell parents</p>
+          <p>Relationships, Anxiety, Depression, Illness, ADHD, loss</p>
+          <p>All trauma issues: Acute, Chronic, or Complex Trauma (PTSD) </p>
+          <p>Long-term effects of growing up with mentally dysfunctional or unwell family...</p>
+          <p>If you are considering psychotherapy/counselling, please email me sds48@icloud.com to arrange an initial phone conversation or visit..</p>
         </div>
       </div>
     </div>
